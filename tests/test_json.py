0e76b1f64c8c1eaa7d831c4a5b07de22f6c4cbc5
brython <brython@localhost>
2013-03-23 18:26:56 +0100

    Update json test

--- tests/test_json.py
+++ tests/test_json.py
@@ -6,4 +6,8 @@ assert json.parse(json.stringify([1,2]))==[1,2]
 assert json.parse(json.stringify({1:2}))=={1:2}
 assert json.parse(json.stringify({1,2}))=={1,2}
 
+xx = '{"status": 0, "result": ["memit/logo00.png", "memit/logo01.png"]}'
+abyss = json.parse(xx)
+assert abyss.result[0]=='memit/logo00.png'
+assert abyss.status==0
 print('all tests ok..')
