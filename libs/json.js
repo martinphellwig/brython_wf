28dd02a56accdc85c6ae67c616406d4e809977dc
brython <brython@localhost>
2013-03-23 18:26:31 +0100

    Fix bug for json.parse

--- libs/json.js
+++ libs/json.js
@@ -5,7 +5,7 @@ $module =  {
     parse : function(json_obj){
         var res = JSON.parse(json_obj)
         var _class = res['class']
-        if(_class===undefined){return res.obj}
+        if(_class===undefined){return $JS2Py(res)}
         else if(['int','str','list'].indexOf(_class)>-1){return res.obj}
         else if(_class==='float'){return res.obj.value}
         else if(_class==='dict'){
