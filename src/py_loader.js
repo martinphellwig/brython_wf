7517992c5fd2de8caa08c7236deddda30e30da13
Billy Earney <billy.earney@gmail.com>
2013-04-20 19:52:51 -0500

    implement 'options' to be passed to brython instead of just 'debug' variable

--- src/py_loader.js
+++ src/py_loader.js
@@ -1,5 +1,5 @@
 // global object with brython built-ins
-$version_info = [1,1,"20130419-191952"]
+$version_info = [1,1,"20130420-194405"]
 
 if(typeof Storage!==undefined){
     var $src = localStorage.getItem('brython')
