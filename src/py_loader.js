b253556c312344dbede778990169e31d652ec11e
brython <brython@localhost>
2013-06-14 09:32:30 +0200

    Introduce function JSContructor : JSContructor(foo)(args) returns JSObject(bar) where bar = new foo(args)

--- src/py_loader.js
+++ src/py_loader.js
@@ -1,5 +1,5 @@
 // global object with brython built-ins
-$version_info = [1,1,"20130611-092246"]
+$version_info = [1,1,"20130614-091528"]
 
 function script_loader(name, version) {
    var $src, $version, $brython_path;
