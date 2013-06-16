fdea5d58efa23db7af8e12607474b4291c20baa7
Billy Earney <billy.earney@gmail.com>
2013-06-16 14:26:44 -0500

    added re modules

--- src/py_loader.js
+++ src/py_loader.js
@@ -1,5 +1,5 @@
 // global object with brython built-ins
-$version_info = [1,1,"20130616-061953"]
+$version_info = [1,1,"20130616-084242"]
 
 function script_loader(name, version) {
    var $src, $version, $brython_path;
