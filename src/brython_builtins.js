b3704facc9ca80735b3ab78957446b66a2f19202
brython <brython@localhost>
2013-06-19 10:35:15 +0200

    Fix bugs in modulo

--- src/brython_builtins.js
+++ src/brython_builtins.js
@@ -29,5 +29,5 @@ if (__BRYTHON__.has_indexedDB) {
 
 __BRYTHON__.re = function(pattern,flags){return JSObject(new RegExp(pattern,flags))}
 __BRYTHON__.has_json = typeof(JSON)!=="undefined"
-__BRYTHON__.version_info = [1,1,"20130616-084242"]
+__BRYTHON__.version_info = [1,1,"20130619-101107"]
 __BRYTHON__.path = [] // path for .py modules
--- src/py_loader.js
+++ src/py_loader.js
@@ -1,5 +1,5 @@
 // global object with brython built-ins
-$version_info = [1,1,"20130616-084242"]
+$version_info = [1,1,"20130619-101107"]
 
 function script_loader(name, version) {
    var $src, $version, $brython_path;
