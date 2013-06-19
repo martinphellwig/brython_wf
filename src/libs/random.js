995dbc99e077417c60196939e9be23aa348a6538
Billy Earney <billy.earney@gmail.com>
2013-06-18 20:02:25 -0500

    add shuffle back in random

--- src/libs/random.js
+++ src/libs/random.js
@@ -1,7 +1,16 @@
 $module = {
     __getattr__ : function(attr){return this[attr]},
     random:function(){return float(Math.random())},
-    randint:function(a,b){return int(Math.floor(Math.random()*(b-a)+a))}
+    randint:function(a,b){return int(Math.floor(Math.random()*(b-a)+a))},
+    shuffle:function(x, rnd){
+      if (x.length <= 1) { return x}
+
+      if (rnd === undefined) {
+         rnd=Math.random
+      }
+
+      for(var j, o, i = x.length; i; j = parseInt(rnd() * i), o = x[--i], x[i] = x[j], x[j] = o);
+    }
 }
 $module.__class__ = $module // defined in $py_utils
 $module.__str__ = function(){return "<module 'random'>"}
