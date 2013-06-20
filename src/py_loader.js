07ba08a19daccfe36f52c8ffc6d560870bc195f6
brython <brython@localhost>
2013-06-20 10:27:59 +0200

    Latest versions of brython.js et al.

--- src/py_loader.js
+++ src/py_loader.js
@@ -1,5 +1,5 @@
 // global object with brython built-ins
-$version_info = [1,1,"20130619-174100"]
+$version_info = [1,1,"20130620-102646"]
 
 function script_loader(name, version) {
    var $src, $version, $brython_path;
