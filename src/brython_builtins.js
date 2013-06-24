c65bd9a3010df0b51f87d4ee4c361154390718d0
brython <brython@localhost>
2013-06-24 18:29:57 +0200

    Latest version of brython.js

--- src/brython_builtins.js
+++ src/brython_builtins.js
@@ -51,5 +51,5 @@ if (__BRYTHON__.has_indexedDB) {
 
 __BRYTHON__.re = function(pattern,flags){return JSObject(new RegExp(pattern,flags))}
 __BRYTHON__.has_json = typeof(JSON)!=="undefined"
-__BRYTHON__.version_info = [1,1,"20130622-222853"]
+__BRYTHON__.version_info = [1,1,"20130624-182502"]
 __BRYTHON__.path = [] // path for .py modules
