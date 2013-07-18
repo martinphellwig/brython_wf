bd71e6d2765366cf3566e8b4cf076fea0523f755
Billy Earney <billy.earney@gmail.com>
2013-07-17 19:38:38 -0500

    fix issue #114

--- src/libs/random.js
+++ src/libs/random.js
@@ -2,7 +2,33 @@ $module = {
     __getattr__ : function(attr){return this[attr]},
     random:function(){return float(Math.random())},
     randint:function(a,b){return int(Math.floor(Math.random()*(b-a)+a))},
-    randrange:function(start,stop){return int(Math.floor(Math.random()*(stop-start)+start))},
+    randrange:function(start,stop,step){
+      if(step === undefined) {
+        step=1;
+      } else if(step == 0) { 
+        //raise ValueError("zero step for randrange()");
+      }
+
+      if(stop === undefined) {
+         stop=start;
+         start=0;
+      }
+      var width=stop-start;
+      if (step==1 && width > 0) {
+        return start + int(Math.floor(Math.random()*width));
+      } else {
+        // raise ValueError("empty range for randrange() ("+start+","+stop+','+step+')');
+      }
+      
+      var n;
+      if (step > 0) {
+         n=Math.floor((width+step-1)/step);
+      } else {
+         n=Math.floor((width+step+1)/step);
+      }
+      return start + step*int(Math.floor(Math.random()*n))
+      //return int(Math.random()*(stop/step-start/step)*step + start)
+    },
     shuffle:function(x, rnd){
       if (x.length <= 1) { return x}
 
