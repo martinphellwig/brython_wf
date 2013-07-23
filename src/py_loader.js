eb20de0ad2c231fcea00921355d12588ddb4b8bb
brython <brython@localhost>
2013-07-23 14:59:44 +0200

    Fixes issue #112 (class namespace)

--- src/py_loader.js
+++ src/py_loader.js
@@ -1,5 +1,5 @@
 // global object with brython built-ins
-$version_info = [1,1,"20130722-100828"]
+$version_info = [1,1,"20130723-144548"]
 
 function script_loader(name, version) {
    var $src, $version, $brython_path;
