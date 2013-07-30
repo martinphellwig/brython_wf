20f199dafef0c483b9710ab86bb8428cda65dee2
brython <brython@localhost>
2013-07-30 08:12:25 +0200

    Fixes issue #118 (exponential notation)

--- src/py_loader.js
+++ src/py_loader.js
@@ -1,5 +1,5 @@
 // global object with brython built-ins
-$version_info = [1,1,"20130729-144527"]
+$version_info = [1,1,"20130730-081138"]
 
 function script_loader(name, version) {
    var $src, $version, $brython_path;
