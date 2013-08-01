ed0a4ff7f85ff0acfcbb1cb2653b2e4e89b9f241
brython <brython@localhost>
2013-08-01 09:46:28 +0200

    Add built-in name __name__ + fic bug in isinstance() for strings

--- src/brython_builtins.js
+++ src/brython_builtins.js
@@ -57,5 +57,5 @@ __BRYTHON__.has_websocket = (function(){
     catch(err){return false}
 })()
 
-__BRYTHON__.version_info = [1,1,"20130731-090941"]
+__BRYTHON__.version_info = [1,1,"20130801-094454"]
 __BRYTHON__.path = [] // path for .py modules
