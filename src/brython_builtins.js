179ea8130a5f3271dd10c92143baa93d03d2d2c4
brython <brython@localhost>
2013-08-01 09:49:35 +0200

    time.js : add struct_time() + add more formatting in strftime()

--- src/brython_builtins.js
+++ src/brython_builtins.js
@@ -57,5 +57,5 @@ __BRYTHON__.has_websocket = (function(){
     catch(err){return false}
 })()
 
-__BRYTHON__.version_info = [1,1,"20130801-094454"]
+__BRYTHON__.version_info = [1,1,"20130801-094851"]
 __BRYTHON__.path = [] // path for .py modules
