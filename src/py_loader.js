57d7926da2dd038fb7d8ff1bd9edcad7ccc96f73
brython <brython@localhost>
2013-08-11 21:13:38 +0200

    Fixes issue #123 : "import a" in __init__.py fails

--- src/py_loader.js
+++ src/py_loader.js
@@ -1,5 +1,5 @@
 // global object with brython built-ins
-$version_info = [1,1,"20130811-173201"]
+$version_info = [1,1,"20130811-211111"]
 
 function script_loader(name, version) {
    var $src, $version, $brython_path;
