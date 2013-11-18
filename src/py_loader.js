5ec69a79ccdba3d43b9942629de7e78f299e3678
brython <brython@localhost>
2013-11-18 23:11:57 +0100

    More work on exceptions

--- src/py_loader.js
+++ src/py_loader.js
@@ -1,5 +1,5 @@
 // global object with brython built-ins
-$version_info = [1,2,"20131118-221505"]
+$version_info = [1,2,"20131118-231032"]
 
 function script_loader(name, version) {
    var $src, $version, $brython_path;
