f7e7b8ef8a137d21d14b9567d8ea32e79305894c
brython <brython@localhost>
2013-11-23 18:27:59 +0100

    Fix bug with try...except...else

--- src/py_loader.js
+++ src/py_loader.js
@@ -1,5 +1,5 @@
 // global object with brython built-ins
-$version_info = [1,2,"20131123-114448"]
+$version_info = [1,2,"20131123-182612"]
 
 function script_loader(name, version) {
    var $src, $version, $brython_path;
