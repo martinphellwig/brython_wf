a90c62e32ab716b1dfe41a48b4727b1f4d9a84b3
Billy Earney <billy.earney@gmail.com>
2013-11-23 07:47:16 -0600

    putting statements back in that work with brython now

--- src/Lib/lib_migration/unittest/util.py
+++ src/Lib/lib_migration/unittest/util.py
@@ -1,8 +1,6 @@
 # "Various utility functions."
 
-#fixme, brython, need to fix namedtuple
-#from collections import namedtuple, OrderedDict
-from collections import OrderedDict
+from collections import namedtuple, OrderedDict
 
 __unittest = True
 
@@ -83,7 +81,7 @@ def three_way_cmp(x, y):
 
 # fixme, brython
 #namedtuple issues in collections.py
-#_Mismatch = namedtuple('Mismatch', 'actual expected value')
+_Mismatch = namedtuple('Mismatch', 'actual expected value')
 
 def _count_diff_all_purpose(actual, expected):
     'Returns list of (cnt_act, cnt_exp, elem) triples where the counts differ'
