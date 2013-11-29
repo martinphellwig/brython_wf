e5bb571c0c4d38fb2e67a2d8675db1f3e24bf588
brython <brython@localhost>
2013-11-29 23:02:08 +0100

    Fix bug with augmented assignment

--- src/py_loader.js
+++ src/py_loader.js
@@ -1,5 +1,5 @@
 // global object with brython built-ins
-$version_info = [1,2,"20131129-221011"]
+$version_info = [1,2,"20131129-230010"]
 
 function script_loader(name, version) {
    var $src, $version, $brython_path;
