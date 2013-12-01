b396aa28970a9404088a48cd2b12d5baff71ecd6
Billy Earney <billy.earney@gmail.com>
2013-12-01 14:18:55 -0600

    fix py_import1.js error in import_test.html

--- src/py_loader.js
+++ src/py_loader.js
@@ -1,5 +1,5 @@
 // global object with brython built-ins
-$version_info = [1,2,"20131129-230918"]
+$version_info = [1,2,"20131201-141640"]
 
 function script_loader(name, version) {
    var $src, $version, $brython_path;
