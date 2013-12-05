70a74879c34480b32b6cd84cf246570422f17403
brython <brython@localhost>
2013-12-05 08:29:29 +0100

    Latest version of brython.js

--- src/version_info.js
+++ src/version_info.js
@@ -1,1 +1,17 @@
-__BRYTHON__.version_info = [1, 3, '20131204-092458', 'alpha', 0]
+__BRYTHON__.version_info = [1, 3, '20131205-082445', 'alpha', 0]
+__BRYTHON__.builtin_module_names = ["posix","builtins",
+    "crypto_js",
+    "hashlib",
+    "javascript",
+    "json",
+    "math",
+    "random",
+    "re",
+    "time",
+    "_ajax",
+    "_browser",
+    "_html",
+    "_os",
+    "_svg",
+    "_timer",
+    "_websocket"]
