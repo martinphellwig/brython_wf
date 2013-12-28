a4c7c4159b352a5cc8529f0280d5ca252945684a
brython <brython@localhost>
2013-12-28 18:14:47 +0100

    Update slideshow module and demo

--- src/Lib/browser/slideshow.py
+++ src/Lib/browser/slideshow.py
@@ -2,17 +2,30 @@ from browser import doc,markdown,html
 
 def show(path,zone,page=0):
     src = open(path).read()
+    title = ''
+    while src.startswith('@'):
+        line_end = src.find('\n')
+        key,value = src[:line_end].split(' ',1)
+        if key=='@title':
+            title = value
+        src = src[line_end+1:]
+    print('title',title)
     zone.html = ''
     pages = src.split('../..\n')
     body = html.DIV()
     body.html = markdown.mark(pages[page])[0]
+
     header = html.DIV()
     previous = html.BUTTON('<',disabled=page==0)
     previous.bind('click',lambda ev:show(path,zone,page-1))
     header <= previous
-    next = html.BUTTON('>',disabled=page==len(pages)-1)
-    next.bind('click',lambda ev:show(path,zone,page+1))
-    header <= next
+    _next = html.BUTTON('>',disabled=page==len(pages)-1)
+    _next.bind('click',lambda ev:show(path,zone,page+1))
+    header <= _next
+
+    footer = html.DIV(Id="footer")
+    if title:
+        footer <= html.DIV(title,Class='title')
         
-    zone <= header+body
+    zone <= header+body+footer
     
