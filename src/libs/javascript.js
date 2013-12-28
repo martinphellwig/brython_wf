d43391e142f0a4f3229622ebb6716db9be75e9a7
Billy Earney <billy.earney@gmail.com>
2013-12-28 13:46:16 -0600

    add drop_files example in gallery, add console function to javascript library

--- src/libs/javascript.js
+++ src/libs/javascript.js
@@ -1,4 +1,5 @@
 $module = {
     JSObject:__BRYTHON__.JSObject,
-    JSConstructor:__BRYTHON__.JSConstructor
+    JSConstructor:__BRYTHON__.JSConstructor,
+    console: __BRYTHON__.JSObject(window.console)
 }
