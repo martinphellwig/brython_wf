2516dd7bc8f574ce87452ee299b5a9ab951693d0
brython <brython@localhost>
2013-12-30 08:27:55 +0100

    Add function "expose" in javascript

--- src/libs/javascript.js
+++ src/libs/javascript.js
@@ -1,5 +1,6 @@
 $module = {
     JSObject:__BRYTHON__.JSObject,
     JSConstructor:__BRYTHON__.JSConstructor,
-    console: __BRYTHON__.JSObject(window.console)
+    console: __BRYTHON__.JSObject(window.console),
+    expose:function(func){window[func.__name__]=func;return func}
 }
