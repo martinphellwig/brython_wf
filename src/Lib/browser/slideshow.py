0b05314aad4896973c58233f27132dd8bd0d946b
brython <brython@localhost>
2013-12-30 14:04:08 +0100

    Improved slideshow

--- src/Lib/browser/slideshow.py
+++ src/Lib/browser/slideshow.py
@@ -6,7 +6,20 @@ def keydown(ev,path,zone,page):
         ev.preventDefault()    
     elif ev.keyCode in [37,38]: #key left or up: previous page
         show(path,zone,page-1)
-        ev.preventDefault()    
+        ev.preventDefault()
+
+def move_to(ev,path,zone,nb_pages):
+    pc = (ev.x-ev.target.left)/ev.target.width
+    page = round(nb_pages*pc)
+    new_pos = '%spx' %(ev.x-ev.target.left-(doc['tl_pos'].width/2))
+    # show page at specified position
+    show(path,zone,page)
+    # set new cursor position
+    doc['tl_pos'].style.left = new_pos
+
+def click_on_tl_pos(ev):
+    # don't move if user clicks on current timeline position
+    ev.stopPropagation()
 
 def show(path,zone,page=0):
     src = open(path).read()
@@ -34,8 +47,15 @@ def show(path,zone,page=0):
 
     footer = html.DIV(Id="footer")
     if title:
-        footer <= html.DIV(title,Class='title')
+        footer <= html.DIV(title,style=dict(display='inline'))
     if page_num:
-        footer <= html.SPAN('%s/%s' %(page+1,len(pages)))
-    zone <= body+footer
+        footer <= html.SPAN(' (%s/%s)' %(page+1,len(pages)),
+            style=dict(display='inline'))
+    timeline = html.DIV(Id='timeline')
+    tl_pos = html.DIV(Id='tl_pos')
+    timeline <= tl_pos
+    timeline.bind('click',lambda ev:move_to(ev,path,zone,len(pages)))
+    tl_pos.bind('click',click_on_tl_pos)
+    zone <= body+footer+timeline
+    tl_pos.style.left = '%spx' %(timeline.width*page/len(pages))
 
