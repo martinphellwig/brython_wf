c1a2bd7f14ae7d1e63c12b33e05a32999bb713ad
brython <brython@localhost>
2014-01-07 20:38:33 +0100

    Add controls to javascript.expose

--- src/libs/javascript.js
+++ src/libs/javascript.js
@@ -2,5 +2,15 @@ $module = {
     JSObject:__BRYTHON__.JSObject,
     JSConstructor:__BRYTHON__.JSConstructor,
     console: __BRYTHON__.JSObject(window.console),
-    expose:function(func){window[func.__name__]=func;return func}
+    expose:function(func){
+        console.log('expose '+(typeof func)+' from module '+func.__module__+' current mod '+__module__)
+        if(typeof func!=='function'){
+            throw TypeError("only functions can be exposed")
+        }
+        if(func.__module__!=='__main__'){
+            throw TypeError("only functions in the main module can be exposed")
+        }
+        window[func.__name__]=func
+        return func
+    }
 }
