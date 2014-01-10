3cdd720686dd69d7949813b8708d4dd42f78cfc5
brython <brython@localhost>
2014-01-10 21:08:23 +0100

    Undo changes to markdown

--- src/Lib/browser/markdown.py
+++ src/Lib/browser/markdown.py
@@ -17,16 +17,8 @@ class CodeBlock:
         self.lines = [line]
     
     def to_html(self):
-        lang = None
-        if self.lines[0].startswith('`'): # indicate language
-            lang = self.lines[0][1:]
-            self.lines = self.lines[1:]
         res = escape('\n'.join(self.lines))
-        #res = unmark(res)
-        if lang == 'python':
-            from browser import syntax_highlight
-            tag = browser.html.PRE(syntax_highlight.colorize(res))
-            res = tag.html
+        res = unmark(res)
         res = '<pre class="marked">%s</pre>\n' %res
         return res,[]
 
