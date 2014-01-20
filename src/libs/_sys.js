af6cacbf02ba5057a792ad11337783adbd1518d4
Billy Earney <billy.earney@gmail.com>
2014-01-19 20:19:40 -0600

    add NotImplementedError functions to _imp.py

--- src/libs/_sys.js
+++ src/libs/_sys.js
@@ -2,11 +2,11 @@ $module = (function($B){
 
     var __builtins__ = $B.builtins
     return {
-        modules : 
+        modules :
             {'__get__':function(){return __builtins__.dict($B.JSObject($B.imported))},
-            '__set__':0 // data descriptor, to force use of __get__
+             '__set__':0 // data descriptor, to force use of __get__
             },
-        stderr : $B.stderr,            
+        stderr : $B.stderr,
         stdout : $B.stdout,
     }
 })(__BRYTHON__)
