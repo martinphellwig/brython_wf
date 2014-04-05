55ce3a6985a6d0acc6b2763d9760b08e70865eee
Billy Earney <billy.earney@gmail.com>
2014-04-05 12:50:10 -0500

    add file keyword argument to print statement

--- site/tests/unittests/HTMLTestRunner.py
+++ site/tests/unittests/HTMLTestRunner.py
@@ -629,7 +629,7 @@ class HTMLTestRunner(Template_mixin):
         test(result)
         self.stopTime = datetime.datetime.now()
         self.generateReport(test, result)
-        print >>sys.stderr, '\nTime Elapsed: %s' % (self.stopTime-self.startTime)
+        print('\nTime Elapsed: %s' % (self.stopTime-self.startTime), file=sys.stderr)
         return result
 
 
